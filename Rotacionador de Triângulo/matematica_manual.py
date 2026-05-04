af9b5096c996ddebf25f9283ac151cd46b984491
JHEMESON DE OLIVEIRA CONDE <125128480+Jhemecon@users.noreply.github.com>
2026-05-04 18:34:52 -0400

    Remove unused matrix functions from matematica_manual.py
    Removed matrix multiplication and 3x3 determinant functions.

--- Rotacionador de Triângulo/matematica_manual.py
+++ Rotacionador de Triângulo/matematica_manual.py
@@ -24,14 +24,6 @@ def existe_triangulo(pontos):
     area_dobrada = x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)
     return abs(area_dobrada) > 1e-9
 
-def multiplicar_matriz_matriz(matriz_a, matriz_b):
-    resultado = [[0, 0], [0, 0]]
-    for i in range(2):
-        for j in range(2):
-            for k in range(2):
-                resultado[i][j] += matriz_a[i][k] * matriz_b[k][j]
-    return resultado
-
 def calcular_determinante_2x2(matriz):
     a = matriz[0][0]
     b = matriz[0][1]
@@ -40,13 +32,3 @@ def calcular_determinante_2x2(matriz):
     
     det = (a * d) - (b * c)
     return round(det, 2)
-
-def calcular_determinante_3x3(matriz):
-    a, b, c = matriz
-    d, e, f = matriz[1]
-    g, h, i = matriz[2]
-    
-    diag_prin = (a * e * i) + (b * f * g) + (c * d * h)
-    diag_sec = (c * e * g) + (a * f * h) + (b * d * i)
-    
-    return round(diag_prin - diag_sec, 2)
